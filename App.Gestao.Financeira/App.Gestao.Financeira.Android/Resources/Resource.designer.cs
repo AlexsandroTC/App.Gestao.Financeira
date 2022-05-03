195bfa055c3594d6d9bb7a81c784d7cf1ac36e83
Alexsandro Teixeira <alexsandro.tcunha@hotmail.com>
2022-05-03 19:43:40 -0300

    Feat: Implementado o visor de saldo em conta.

--- App.Gestao.Financeira/App.Gestao.Financeira.Android/Resources/Resource.designer.cs
+++ App.Gestao.Financeira/App.Gestao.Financeira.Android/Resources/Resource.designer.cs
@@ -22251,16 +22251,25 @@ namespace App.Gestao.Financeira.Droid
 			public const int preference_list_divider_material = 2131165332;
 			
 			// aapt resource value: 0x7F070095
-			public const int seta_baixo_cinza = 2131165333;
+			public const int refresh = 2131165333;
 			
 			// aapt resource value: 0x7F070096
-			public const int test_custom_background = 2131165334;
+			public const int refresh2x = 2131165334;
 			
 			// aapt resource value: 0x7F070097
-			public const int tooltip_frame_dark = 2131165335;
+			public const int refresh3x = 2131165335;
 			
 			// aapt resource value: 0x7F070098
-			public const int tooltip_frame_light = 2131165336;
+			public const int seta_baixo_cinza = 2131165336;
+			
+			// aapt resource value: 0x7F070099
+			public const int test_custom_background = 2131165337;
+			
+			// aapt resource value: 0x7F07009A
+			public const int tooltip_frame_dark = 2131165338;
+			
+			// aapt resource value: 0x7F07009B
+			public const int tooltip_frame_light = 2131165339;
 			
 			static Drawable()
 			{
